b5f563c38cfef8a947d6788beadbb2b9e1d22f56
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-09-22 20:56:06 -0400

    download the raw data from open data toronto

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,44 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto
+# Author: Talia Fabregas
+# Date: 22 September 2024
+# Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+# install.packages("readxl")
+library(readxl)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+
+# subway_2018 <- list_package_resources("996cfe8d-fb35-40ce-b569-698d51fc683b") |>
+#   filter(name == "ttc-subway-delay-data-2018")
+# 
+# subway_2019 <- list_package_resources("996cfe8d-fb35-40ce-b569-698d51fc683b") |>
+#   filter(name == "ttc-subway-delay-data-2019")
+# 
+# subway_2020 <- list_package_resources("996cfe8d-fb35-40ce-b569-698d51fc683b") |>
+#   filter(name == "ttc-subway-delay-data-2020")
+# 
+# subway_2021 <- list_package_resources("996cfe8d-fb35-40ce-b569-698d51fc683b") |>
+#   filter(name == "ttc-subway-delay-data-2021")
+# 
+# subway_2022 <- list_package_resources("996cfe8d-fb35-40ce-b569-698d51fc683b") |>
+#   filter(name == "ttc-subway-delay-data-2022")
+
+subway_2023 <- list_package_resources("996cfe8d-fb35-40ce-b569-698d51fc683b") |>
+  filter(name == "ttc-subway-delay-data-2023") |>
+  get_resource()
+
 
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write.csv(subway_2023, "data/raw_data/subway_2023.csv") 
 
          
