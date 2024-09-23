3e6700907c551e5ca120c330870c04d2698a3cc3
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-09-22 20:37:18 -0400

    simulate the data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,42 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates TTC Subway Delays in 2023
+# Author: Talia Fabregas
+# Date: 11 21 September 2024
+# Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+set.seed(304)
 
-#### Simulate data ####
-# [...ADD CODE HERE...]
+#### Simulate Data
 
+# Define a start and end date for 2023
+start_date <- as.Date("2023-01-01")
+end_date <- as.Date("2023-12-31")
 
+subway_stations <- c(
+  "Vaughan Metropolitan Centre", "Hwy 407", "Pioneer Village", "York University",
+  "Finch West", "Downsview Park", "Sheppard West", "Wilson", "Yorkdale",
+  "Lawrence West", "Glencairn", "Eglington West", "St. Clair West", "Dupont",
+  "Spadina", "St. George", "Museum", "Queen's Park", "St. Patrick", "Osgoode",
+  "St. Andrew", "Union", "King", "Queen", "Dundas", "College", "Wellsley", 
+  "Bloor Yonge", "Summerhill", "Rosedale", "St. Clair", "Davisville",
+  "Eglington", "Lawrence", "York Mills", "Sheppard Yonge", "Finch")
+
+delay_reasons <- c("Mechanical Issue", "Trespasser on Tracks", "Weather",
+                   "Operational Issue", "Signal Issue", "Power Outage")
+num_sims = 500
+
+simulated_data <- tibble(
+  # Simulate 500 subway delays
+  date = sample(seq.Date(start_date, end_date, by = "day"), num_sims, replace=TRUE),
+  station = sample(subway_stations, num_sims, replace=TRUE),
+  start_time = sprintf("%02d:%02d", sample(0:23, num_sims, replace=TRUE), sample(0:59, num_sims, replace=TRUE)),
+  duration = sample(2:60, num_sims, replace = TRUE),
+  reason = sample(delay_reasons, num_sims, replace=TRUE)
+)
 
